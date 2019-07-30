2755359030e46b7e35d49202091008ebc441beb0
xiaomingplus <returnyang@gmail.com>
2019-07-30 11:19:43 +0800

    deploy

---
+++ precache-manifest.52236c2d9d8bd75d3114eb18748f3f8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e331f54dde916f5bca0e40ed24771b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49079a84d95430668cbf",
+    "url": "/static/css/main.82d288d2.chunk.css"
+  },
+  {
+    "revision": "255bc4320f7e64c39a97",
+    "url": "/static/js/2.ac79aae4.chunk.js"
+  },
+  {
+    "revision": "49079a84d95430668cbf",
+    "url": "/static/js/main.e9df08dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
